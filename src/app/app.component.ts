8c43ba7f4bf55ebd01af2ca7dc2b6e77404259dd
kivancaksoy <kvnc96@hotmail.com>
2023-02-14 23:59:01 +0300

    CustomToastrService added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $: any;
 @Component({
   selector: 'app-root',
@@ -9,9 +10,11 @@ declare var $: any;
 export class AppComponent {
   title = 'ECommerceClient';
 
-  constructor(private toastr: ToastrService) {
-
-    toastr.success("Merhaba", "Kıvanç");
+  constructor(private toastrService: CustomToastrService) {
+    toastrService.message("Merhaba", "Kıvanç", { messageType: ToastrMessageType.Info, position: ToastrPosition.TopCenter });
+    toastrService.message("Merhaba", "Kıvanç", { messageType: ToastrMessageType.Success, position: ToastrPosition.BottomFullWidth });
+    toastrService.message("Merhaba", "Kıvanç", { messageType: ToastrMessageType.Error, position: ToastrPosition.TopLeft });
+    toastrService.message("Merhaba", "Kıvanç", { messageType: ToastrMessageType.Warning, position: ToastrPosition.TopRight });
     
   }
 }
