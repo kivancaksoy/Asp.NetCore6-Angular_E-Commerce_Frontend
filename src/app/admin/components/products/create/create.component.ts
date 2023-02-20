4d970f8e991a44f0f39d9c11b92520cd24b1760e
kivancaksoy <kvnc96@hotmail.com>
2023-02-20 15:04:02 +0300

    FileUploadComponent added. ngx-file-drop added.

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -1,44 +1,67 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { FileUploadOptions } from 'src/app/services/common/file-upload/file-upload.component';
 import { BaseComponent, SpinnerType } from '../../../../base/base.component';
 import { Create_Product } from '../../../../contracts/create_product';
-import { AlertifyService, MessageType, Position } from '../../../../services/admin/alertify.service';
+import {
+  AlertifyService,
+  MessageType,
+  Position,
+} from '../../../../services/admin/alertify.service';
 import { ProductService } from '../../../../services/common/models/product.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent extends BaseComponent {
-  constructor(private productService: ProductService, spinner: NgxSpinnerService, private alertify: AlertifyService) {
+  constructor(
+    private productService: ProductService,
+    spinner: NgxSpinnerService,
+    private alertify: AlertifyService
+  ) {
     super(spinner);
   }
 
-  @Output() createdProduct : EventEmitter<Create_Product> = new EventEmitter();
+  @Output() createdProduct: EventEmitter<Create_Product> = new EventEmitter();
+  @Output() fileUploadOptions: Partial<FileUploadOptions> = {
+    action: 'upload',
+    controller: 'products',
+    explanation: 'Resimleri sürükleyin veya seçin...',
+    isAdminPage: true,
+    accept: ".png, .jpg, .jpeg, .txt"
+  };
 
-  create(name: HTMLInputElement, stock: HTMLInputElement, price: HTMLInputElement) {
+  create(
+    name: HTMLInputElement,
+    stock: HTMLInputElement,
+    price: HTMLInputElement
+  ) {
     this.showSpinner(SpinnerType.BallAtom);
-    const create_product: Create_Product = new Create_Product;
+    const create_product: Create_Product = new Create_Product();
     create_product.name = name.value;
     create_product.stock = parseInt(stock.value);
     create_product.price = parseFloat(price.value);
 
-    
-    this.productService.create(create_product, () => {
-      this.hideSpinner(SpinnerType.BallAtom);
-      this.alertify.message("Ürün başarıyla eklenmiştir.", {
-        dismissOthers: true,
-        messageType: MessageType.Success,
-        position: Position.TopRight
-      });
-      this.createdProduct.emit(create_product);
-    }, errorMessage => {
-      this.alertify.message(errorMessage, {
-        dismissOthers: true,
-        messageType: MessageType.Error,
-        position: Position.TopRight
-      });
-    });
+    this.productService.create(
+      create_product,
+      () => {
+        this.hideSpinner(SpinnerType.BallAtom);
+        this.alertify.message('Ürün başarıyla eklenmiştir.', {
+          dismissOthers: true,
+          messageType: MessageType.Success,
+          position: Position.TopRight,
+        });
+        this.createdProduct.emit(create_product);
+      },
+      (errorMessage) => {
+        this.alertify.message(errorMessage, {
+          dismissOthers: true,
+          messageType: MessageType.Error,
+          position: Position.TopRight,
+        });
+      }
+    );
   }
 }
